44a74eabe805d5b69ea654fc3607940daa0b2973
ngwali <ngwaliabuubakar@gmail.com>
2024-05-07 20:33:53 +0530

    Update Task_05.py

--- Task_05.py
+++ Task_05.py
@@ -1,12 +1,1 @@
-from scapy.all import sniff
 
-def packet_callback(packet):
-    if packet.haslayer('IP'):
-        src_ip = packet['IP'].src
-        dst_ip = packet['IP'].dst
-        proto = packet['IP'].proto
-        payload = packet.payload.load if packet.payload else None
-        print(f"Source IP: {src_ip}, Destination IP: {dst_ip}, Protocol: {proto}, Payload: {payload}")
-
-# the code must be able to target and Sniff packets on the network interface 'Ethernet 3'
-sniff(iface='Ethernet 3', prn=packet_callback, store=0)
